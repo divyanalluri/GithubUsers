92eb979e01a2e6e9e907d3849432705fb336f53d
prasanth-277 <prasanthreddy549@gmail.com>
2019-08-22 18:41:45 +0530

    cleared review inputs

--- src/components/UserRepos/UserRepo/index.js
+++ src/components/UserRepos/UserRepo/index.js
@@ -14,17 +14,19 @@ const width = Dimensions.get('window').width;
 
 export default class UserRepo extends Component {
   render() {
+    const {repo} = this.props;
+
     return (
       <RepoContainer width={width}>
-        <RepoText>{this.props.repo.name}</RepoText>
+        <RepoText>{repo.name}</RepoText>
         <FeatureBox>
           <Features>
             <Icon source={require('./star.png')} />
-            <Property>Stars: {this.props.repo.starsCount} </Property>
+            <Property>Stars: {repo.starsCount} </Property>
           </Features>
           <Features>
             <Icon source={require('./fork.png')} />
-            <Property>Forks: {this.props.repo.forksCount}</Property>
+            <Property>Forks: {repo.forksCount}</Property>
           </Features>
         </FeatureBox>
       </RepoContainer>
